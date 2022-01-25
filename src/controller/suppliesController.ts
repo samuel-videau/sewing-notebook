1474db589dafc0de2e60883d90bb1e0b94ebdeb6
Samuel VIDEAU <samuel.videau@efrei.net>
2022-01-25 10:04:43 +0100

    feat: update and delete supply

--- src/controller/suppliesController.ts
+++ src/controller/suppliesController.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import {Supply} from "../schemas/types/supply";
 import * as admin from 'firebase-admin';
 import * as supplySchema from '../schemas/json/supply.json';
+import * as optionalSupplySchema from '../schemas/json/optional-supply.json';
 
 const suppliesParamsSchema = {
   type: 'object',
@@ -73,9 +74,10 @@ export async function suppliesController (fastify: FastifyInstance) {
       try {
         const { supplyId } = request.params as { supplyId: string };
         const doc = await supplyCollection.doc(supplyId).get();
+        if (!doc.data()) return reply.code(404).send('Supply not found');
         return reply.code(200).send(JSON.stringify(doc.data() as Supply));
       } catch (e: any) {
-        return reply.code(404).send('Supply not found');
+        return reply.code(500).send('Internal Error');
       }
     }
   });
@@ -84,16 +86,16 @@ export async function suppliesController (fastify: FastifyInstance) {
     method: 'PUT',
     url: '/:supplyId',
     schema: {
-      body: { 200: supplySchema },
+      body: optionalSupplySchema,
       params: suppliesParamsSchema
       },
     handler: async (request, reply) => {
       try {
         const { supplyId } = request.params as { supplyId: string };
-        await supplyCollection.doc(supplyId).set(request.body);
+        await supplyCollection.doc(supplyId).update(request.body);
         await reply.code(200).send();
       } catch (e: any) {
-        return reply.code(404).send('Supply not found');
+        return reply.code(500).send('Internal Error');
       }
     }
   });
@@ -108,10 +110,9 @@ export async function suppliesController (fastify: FastifyInstance) {
       try {
         const { supplyId } = request.params as { supplyId: string };
         await supplyCollection.doc(supplyId).delete();
-
         await reply.code(200).send();
       } catch (e: any) {
-        return reply.code(404).send('Supply not found');
+        return reply.code(500).send('Internal Error');
       }
     }
 });
