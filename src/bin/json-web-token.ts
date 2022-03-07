c99f9f53bb6c404bf18b4679cdc82e6e2f9e5f06
Samuel VIDEAU <samuel.videau@efrei.net>
2022-03-07 19:21:23 +0100

    feat: verifyJWT function

--- src/bin/json-web-token.ts
+++ src/bin/json-web-token.ts
@@ -3,6 +3,7 @@ import {JWT_SECRET} from '../environment/endpoints';
 import {FastifyReply, FastifyRequest} from 'fastify';
 import {error, ERROR_JWT_EXPIRED, ERROR_NO_JWT_TOKEN, ERROR_UNAUTHORIZED} from './utils/error-messages';
 import {JWT_VALIDITY_TIME} from "../environment/config";
+import {queryProjectAuth} from "./DB/project-auth.table";
 
 export interface JWTPayload {
   userId: string,
@@ -16,9 +17,9 @@ export function generateJWT(userId: string): string {
 	}, JWT_SECRET, {expiresIn: JWT_VALIDITY_TIME.toString() + 'h'});
 }
 
-export function verifyJWT(req: FastifyRequest, res: FastifyReply, userId: string) {
+export async function verifyJWT(req: FastifyRequest, res: FastifyReply, projectId: string) {
 	if (!req.headers.authorization) return res.code(401).send(error(401, ERROR_NO_JWT_TOKEN));
 	const payload: JWTPayload = jwt.verify(req.headers.authorization?.split(' ')[1], JWT_SECRET) as JWTPayload;
-	if (userId !== payload.userId) return res.code(403).send(error(403, ERROR_UNAUTHORIZED));
+	if (!await queryProjectAuth(payload.userId, projectId)) return res.code(403).send(error(403, ERROR_UNAUTHORIZED));
 	if (payload.exp < parseInt((new Date()).getTime().toString().slice(0, 10))) return res.code(401).send(error(401, ERROR_JWT_EXPIRED));
 }
