c5893c5f740d0c6fcd13c62bd767d47761735382
Samuel VIDEAU <samuel.videau@efrei.net>
2022-03-07 18:12:31 +0100

    feat: JWT service

---
+++ src/bin/json-web-token.ts
@@ -0,0 +1,24 @@
+import * as jwt from 'jsonwebtoken';
+import {JWT_SECRET} from '../environment/endpoints';
+import {FastifyReply, FastifyRequest} from 'fastify';
+import {error, ERROR_JWT_EXPIRED, ERROR_NO_JWT_TOKEN, ERROR_UNAUTHORIZED} from './utils/error-messages';
+import {JWT_VALIDITY_TIME} from "../environment/config";
+
+export interface JWTPayload {
+  userId: string,
+  iat: number,
+  exp: number
+}
+
+export function generateJWT(userId: string): string {
+	return  jwt.sign({
+		userId
+	}, JWT_SECRET, {expiresIn: JWT_VALIDITY_TIME.toString() + 'h'});
+}
+
+export function verifyJWT(req: FastifyRequest, res: FastifyReply, userId: string) {
+	if (!req.headers.authorization) return res.code(401).send(error(401, ERROR_NO_JWT_TOKEN));
+	const payload: JWTPayload = jwt.verify(req.headers.authorization?.split(' ')[1], JWT_SECRET) as JWTPayload;
+	if (userId !== payload.userId) return res.code(403).send(error(403, ERROR_UNAUTHORIZED));
+	if (payload.exp < parseInt((new Date()).getTime().toString().slice(0, 10))) return res.code(401).send(error(401, ERROR_JWT_EXPIRED));
+}
